92ebad350f43529e7d66a8f6bff4965ac15c0b47
Matt Johnson <mattjohnson@arcbtech.com>
2024-05-31 11:58:19 -0500

    finished skills without the links

--- ResumeSite/src/components/SkillsPage.vue
+++ ResumeSite/src/components/SkillsPage.vue
@@ -213,12 +213,211 @@
                 dark
                 >
                 <div class="aboutBtn">
-                    <v-icon>mdi-linux</v-icon> Linux System Administration
+                    <v-icon>mdi-dot-net</v-icon> .NET Core
+                </div>
+            </v-btn>
+        </a>
+        </v-col>
+        <v-col cols="auto">
+        <a style="text-decoration: none; color: inherit;" href="https://vuetifyjs.com/en/introduction/why-vuetify/#what-is-vuetify3f" target="_blank" rel="noopener noreferrer">
+            <v-btn  
+                style="background-color: #55a176"
+                rounded="xl" 
+                size="x-large" 
+                color="#55a176"
+                dark
+                >
+                <div class="aboutBtn">
+                    <v-icon>mdi-dot-net</v-icon> .NET Framework
+                </div>
+            </v-btn>
+        </a>
+        </v-col>
+        <v-col cols="auto">
+        <a style="text-decoration: none; color: inherit;" href="https://vuetifyjs.com/en/introduction/why-vuetify/#what-is-vuetify3f" target="_blank" rel="noopener noreferrer">
+            <v-btn  
+                style="background-color: #55a176"
+                rounded="xl" 
+                size="x-large" 
+                color="#55a176"
+                dark
+                >
+                <div class="aboutBtn">
+                    <v-icon>mdi-code-json</v-icon> JSON
                 </div>
             </v-btn>
         </a>
         </v-col>
     </v-row>
+    <v-row>
+        <v-col cols="1"></v-col>
+        <v-col cols="auto">
+        <a style="text-decoration: none; color: inherit;" href="https://vuetifyjs.com/en/introduction/why-vuetify/#what-is-vuetify3f" target="_blank" rel="noopener noreferrer">
+            <v-btn  
+                style="background-color: #55a176"
+                rounded="xl" 
+                size="x-large" 
+                color="#55a176"
+                dark
+                >
+                <div class="aboutBtn">
+                    <v-icon>mdi-linux</v-icon> Linux System Administration
+                </div>
+            </v-btn>
+        </a>
+        </v-col>
+        <v-col cols="auto">
+        <a style="text-decoration: none; color: inherit;" href="https://vuetifyjs.com/en/introduction/why-vuetify/#what-is-vuetify3f" target="_blank" rel="noopener noreferrer">
+            <v-btn  
+                style="background-color: #55a176"
+                rounded="xl" 
+                size="x-large" 
+                color="#55a176"
+                dark
+                >
+                <div class="aboutBtn">
+                    <v-icon>mdi-database</v-icon> DB2
+                </div>
+            </v-btn>
+        </a>
+        </v-col>
+        <v-col cols="auto">
+        <a style="text-decoration: none; color: inherit;" href="https://vuetifyjs.com/en/introduction/why-vuetify/#what-is-vuetify3f" target="_blank" rel="noopener noreferrer">
+            <v-btn  
+                style="background-color: #55a176"
+                rounded="xl" 
+                size="x-large" 
+                color="#55a176"
+                dark
+                >
+                <div class="aboutBtn">
+                    <v-icon>mdi-security</v-icon> Auth0
+                </div>
+            </v-btn>
+        </a>
+        </v-col>
+        <v-col cols="auto">
+        <a style="text-decoration: none; color: inherit;" href="https://vuetifyjs.com/en/introduction/why-vuetify/#what-is-vuetify3f" target="_blank" rel="noopener noreferrer">
+            <v-btn  
+                style="background-color: #55a176"
+                rounded="xl" 
+                size="x-large" 
+                color="#55a176"
+                dark
+                >
+                <div class="aboutBtn">
+                    <v-icon>mdi-security</v-icon> OAuth 2.0
+                </div>
+            </v-btn>
+        </a>
+        </v-col>
+        <v-col cols="auto">
+        <a style="text-decoration: none; color: inherit;" href="https://vuetifyjs.com/en/introduction/why-vuetify/#what-is-vuetify3f" target="_blank" rel="noopener noreferrer">
+            <v-btn  
+                style="background-color: #55a176"
+                rounded="xl" 
+                size="x-large" 
+                color="#55a176"
+                dark
+                >
+                <div class="aboutBtn">
+                    <v-icon>mdi-microsoft</v-icon> Azure DevOps
+                </div>
+            </v-btn>
+        </a>
+        </v-col>
+        <v-col cols="auto">
+        <a style="text-decoration: none; color: inherit;" href="https://vuetifyjs.com/en/introduction/why-vuetify/#what-is-vuetify3f" target="_blank" rel="noopener noreferrer">
+            <v-btn  
+                style="background-color: #55a176"
+                rounded="xl" 
+                size="x-large" 
+                color="#55a176"
+                dark
+                >
+                <div class="aboutBtn">
+                    <v-icon>mdi-reiterate</v-icon> Agile
+                </div>
+            </v-btn>
+        </a>
+        </v-col>
+        <v-col cols="auto">
+        <a style="text-decoration: none; color: inherit;" href="https://vuetifyjs.com/en/introduction/why-vuetify/#what-is-vuetify3f" target="_blank" rel="noopener noreferrer">
+            <v-btn  
+                style="background-color: #55a176"
+                rounded="xl" 
+                size="x-large" 
+                color="#55a176"
+                dark
+                >
+                <div class="aboutBtn">
+                    <v-icon>mdi-unity</v-icon> Unity
+                </div>
+            </v-btn>
+        </a>
+        </v-col>
+    </v-row>
+
+    <v-row>
+        <v-col cols="2"></v-col>
+        <v-col cols="auto">
+        <a style="text-decoration: none; color: inherit;" href="https://vuetifyjs.com/en/introduction/why-vuetify/#what-is-vuetify3f" target="_blank" rel="noopener noreferrer">
+            <v-btn  
+                style="background-color: #55a176"
+                rounded="xl" 
+                size="x-large" 
+                color="#55a176"
+                dark
+                >
+                <div class="aboutBtn">
+                    <v-icon>mdi-monitor</v-icon> Computational Theory
+                </div>
+            </v-btn>
+        </a>
+        </v-col>
+        <v-col cols="auto">
+        <a style="text-decoration: none; color: inherit;" href="https://vuetifyjs.com/en/introduction/why-vuetify/#what-is-vuetify3f" target="_blank" rel="noopener noreferrer">
+            <v-btn  
+                style="background-color: #55a176"
+                rounded="xl" 
+                size="x-large" 
+                color="#55a176"
+                dark
+                >
+                <div class="aboutBtn">
+                    <v-icon>mdi-account-tie</v-icon> Executive Management
+                </div>
+            </v-btn>
+        </a>
+        </v-col>
+        <v-col cols="auto">
+            <v-btn  
+                style="background-color: #55a176"
+                rounded="xl" 
+                size="x-large" 
+                color="#55a176"
+                dark
+                >
+                <div class="aboutBtn">
+                    <v-icon>mdi-arrange-bring-forward</v-icon> Project Management
+                </div>
+            </v-btn>
+        </v-col>
+        <v-col cols="auto">
+            <v-btn  
+                style="background-color: #55a176"
+                rounded="xl" 
+                size="x-large" 
+                color="#55a176"
+                dark
+                >
+                <div class="aboutBtn">
+                    <v-icon>mdi-poll</v-icon> Market Analytics
+                </div>
+            </v-btn>
+        </v-col>
+    </v-row>
+
     <br><br>
     
 </template>
